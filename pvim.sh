604b268a24fa9a815d6e7272be1c1739f1878bbf
VampireSH <sherlockya@gmail.com>
2017-01-07 02:14:25 +0800

    完善了复制功能

--- pvim.sh
+++ pvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cat $1 | curl -F 'vimcn=<-' http://cfp.vim-cn.com/ | tee /dev/stderr | xclip -sel c -i
+cat $1 | curl -F 'vimcn=<-' http://cfp.vim-cn.com/ | tee /dev/stderr | head -c -1 |xclip -sel c -i
 
-# echo "已复制cfp-vim的URL到剪贴板"  已废弃
+# tee /dev/stderr 是为了输出到屏幕
+# head -c -1 是为了去除粘贴时那奇怪的换行
+# xclip -sel c -i 复制到剪贴板
