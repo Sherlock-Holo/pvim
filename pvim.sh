fafd934210047ecebd1f38d84638f9b72062d2be
VampireSH <sherlockya@gmail.com>
2017-01-07 02:03:17 +0800

    现在可以复制到剪贴板的同时输出了

--- pvim.sh
+++ pvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat $1 | curl -F 'vimcn=<-' http://cfp.vim-cn.com/
+cat $1 | curl -F 'vimcn=<-' http://cfp.vim-cn.com/ | tee /dev/stderr | xclip -sel c -i
 
 # echo "已复制cfp-vim的URL到剪贴板"  已废弃
