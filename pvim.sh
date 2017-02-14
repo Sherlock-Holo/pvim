35c352fb3f688df1e986b38646f9ab583a6f937a
VampireSH <sherlockya@gmail.com>
2017-02-14 23:13:47 +0800

    添加上传图片功能

--- pvim.sh
+++ pvim.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cat $1 | curl -F 'vimcn=<-' http://cfp.vim-cn.com/ | tee /dev/stderr | head -c -1 |xclip -sel c -i
+Input=$1
+Type=$2
 
+if [ $Type == "codes" ]; then
+    cat $Input | curl -F 'vimcn=<-' https://cfp.vim-cn.com/ | tee /dev/stderr | head -c -1 |xclip -sel c -i
+fi
+
+if [ $Type == "pics" ]; then
+        curl -F 'name=@'${Input}'' https://img.vim-cn.com/ | tee /dev/stderr | head -c -1 |xclip -sel c -i
+fi
 # tee /dev/stderr 是为了输出到屏幕
 # head -c -1 是为了去除粘贴时那奇怪的换行
 # xclip -sel c -i 复制到剪贴板
