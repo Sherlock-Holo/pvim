6486d634c9a8cec6b41bae3eb2b7acc44c590476
VampireSH <sherlockya@gmail.com>
2017-05-14 21:40:37 +0800

    add hidden usage

--- pvim2.py
+++ pvim2.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-import requests
 import argparse
 import pyperclip
+import requests
+import sys
 from configparser import ConfigParser
 from time import time
 
@@ -17,7 +18,7 @@ parameter = cfg.get(server, 'arg')
 def upload_img(file, arg):
     with open(file, 'rb') as f:
         start_time = time()
-        ufile = requests.post(img_server, files = {arg: f.read(), 'content_type': 'application/octet-stream'})
+        ufile = requests.post(img_server, files={arg: f.read(), 'content_type': 'application/octet-stream'})
         end_time = time()
         url = ufile.text.split()[-1]
         usage_time = round(end_time - start_time, 2)
@@ -30,7 +31,7 @@ def upload_text(file, arg):
     postfix = file.split('.')[-1]
     with open(file, 'r') as f:
         start_time = time()
-        ufile = requests.post(text_server, data = {arg: f.read(),  'content_type': 'application/octet-stream'})
+        ufile = requests.post(text_server, data={arg: f.read(),  'content_type': 'application/octet-stream'})
         end_time = time()
         url = ufile.text
         url = url.strip()
@@ -40,10 +41,23 @@ def upload_text(file, arg):
         pyperclip.copy(url)
         return url
 
+def upload_pipe_test(arg):
+    args = sys.stdin
+    start_time = time()
+    ufile = requests.post(text_server, data={arg: args.read(),  'content_type': 'application/octet-stream'})
+    end_time = time()
+    url = ufile.text
+    url = url.strip()
+    usage_time = round(end_time - start_time, 2)
+    print('upload time: {}s'.format(usage_time))
+    pyperclip.copy(url)
+    return url
+
 # opt
-parser = argparse.ArgumentParser(description = 'pvim python3 version')
-parser.add_argument('-t', '--text', help = 'upload codes text')
-parser.add_argument('-p', '--picture', help = 'upload picture')
+parser = argparse.ArgumentParser(description='pvim python3 version')
+parser.add_argument('-t', '--text', help='upload codes text')
+parser.add_argument('-p', '--picture', help='upload picture')
+#parser.add_argument('--pipe',action='store_true' , help='upload from pipe')
 
 args = parser.parse_args()
 
@@ -52,3 +66,6 @@ if args.picture:
 
 elif args.text:
     print(upload_text(args.text, parameter))
+
+else:
+    print(upload_pipe_test(parameter))
